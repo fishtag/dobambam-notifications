56b06bbf16c1fea92455da5c5fed0dfd27ab61d4
Timur Khafizov <timur.khafizov@fishtag.ru>
2014-08-28 22:36:57 +0400

    Add association for user and endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :endpoints
+
   validates :full_name, presence: true
 
   def to_s
